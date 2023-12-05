e9a69a42d677320ddc48bd786df8fcb657a7a304
wynne <khaiyee.tan@gmail.com>
2023-12-05 20:38:09 +0800

    init project

---
+++ src/main/java/com/kwan/pet_item_service/PetItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kwan.pet_item_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwan/pet_item_service/PetItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwan.pet_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
